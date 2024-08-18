280a28d52a83d47a87b4a69a4aff07d578b6a8d4
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-08-18 12:40:42 +0800

    Components (Login form)
    Creating Login form for OSA and Student Account (Front End)

---
+++ components/osa_login_form.php
@@ -0,0 +1,33 @@
+<div class="parent-container">
+    <div class="login-form-container">
+        <div class="login_form_card">
+            <div class="login-text-title">
+                <h2>Login</h2>
+                <h4>Welcome Back, Please Login To Your OSA Account.</h4>
+            </div>
+
+            <div class="text-box-container">
+                <div class="text-box">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="text-box">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <div class="links-container">
+                    <label>
+                        <input type="checkbox" name="remember_me">
+                        Remember me
+                    </label>
+                    <a href="#" class="forgot-password">Forgot Password?</a>
+                </div>
+
+                <div class="btn-container">
+                    <button class="login-btn">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
