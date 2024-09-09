60777cf0614205e4137cfa5e35c4209c218db96b
GerodiazRogel-BSIT32E1 <160821545+GerodiazRogel-BSIT32E1@users.noreply.github.com>
2024-09-09 13:02:48 +0800

    searchbar
    searchbar for Student Profile module

---
+++ javascript/searchbar.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".searchbar").on("keyup change", function() {
+        var searchValue = $(this).val().toLowerCase();
+        // console.log("Search Value:", searchValue);
+
+        $("table tbody tr").each(function() {
+            var row = $(this).text().toLowerCase();
+            var match = row.indexOf(searchValue) > -1;
+            // console.log("Row:", row, "Match:", match);
+            $(this).toggle(match);
+        });
+    });
+});
