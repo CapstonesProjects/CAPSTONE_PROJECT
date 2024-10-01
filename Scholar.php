5c30d6f14689fcbf663a5c99ba7866df9ad1865c
Perdon-ShannaMae-BSIT32E1 <145529377+Perdon-ShannaMae-BSIT32E1@users.noreply.github.com>
2024-10-01 10:05:58 +0800

    SR

--- Scholar.php
+++ Scholar.php
@@ -24,10 +24,20 @@
         <div class="scholar-container">
             <d class="bg-container">
                 <h1 class="scholar-title">Scholarships and Grants</h1>
-                Entrance Scholarships (For New Students) - are extended to valedictorians</br> 
+                <p>Entrance Scholarships (For New Students) - are extended to valedictorians</br> 
                 and salutatorians of all secondary schools recognized by the Department of Education. An</br>
                 academic Scholarship intended for new students is a scholarship grant given to</br> 
                 incoming freshmen.</p>
+                <p>An examination is being given to high school graduates whose grades in all subjects
+                during the 4th grading period of their last year in high school are not lower than 85%. 
+                However, consideration is being given to students who have a single grade lower than 85% 
+                in a particular subject while the remaining subjects fully compensate for the deficit. In such 
+                cases, the Accounting Office verbally informs the applicant that he/she will be allowed to
+                take the scholarship exam, provided that his/her raw score shall not be lower than 84 points</p>
+                <p>If he/she scores 84 points below, the minimum discount will automatically be given to
+                any student who took the initiative to take the examination. This scholarship grant is good
+                for 1 semester only.</p>
+
             </div>
         </div>
     </section>
