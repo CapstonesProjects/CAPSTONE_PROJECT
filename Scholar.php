71b1f0aeb3f71832661074fb1ab1954f409af9bc
Perdon-ShannaMae-BSIT32E1 <145529377+Perdon-ShannaMae-BSIT32E1@users.noreply.github.com>
2024-10-02 10:01:11 +0800

    2

--- Scholar.php
+++ Scholar.php
@@ -24,6 +24,7 @@
         <div class="scholar-container">
             <div class="bg-container">
             <div class="dep-img">
+                <h2>Departments</h2>
             <img src="images/Scho/CCS.png" class="dep-img-item">
                 <img src="images/Scho/CE.png" class="dep-img-item">
                 <img src="images/Scho/CCJ.png" class="dep-img-item">
@@ -34,22 +35,41 @@
                 <img src="images/Scho/BME.png" class="dep-img-item">
                 <img src="images/Scho/BE.png" class="dep-img-item">
                 </div>
+
                 <div class="text-content">
                 <h1 class="scholar-title">Scholarships and Grants</h1>
                 
-                <p>Entrance Scholarships (For New Students) - are extended to valedictorians</br> 
-                and salutatorians of all secondary schools recognized by the Department of Education. An</br>
-                academic Scholarship intended for new students is a scholarship grant given to</br> 
-                incoming freshmen.</p>
-                <p>An examination is being given to high school graduates whose grades in all subjects</br> 
-                during the 4th grading period of their last year in high school are not lower than 85%. </br> 
-                However, consideration is being given to students who have a single grade lower than 85% </br> 
-                in a particular subject while the remaining subjects fully compensate for the deficit. In such </br> 
-                cases, the Accounting Office verbally informs the applicant that he/she will be allowed to</br> 
-                take the scholarship exam, provided that his/her raw score shall not be lower than 84 points</br> </p>
-                <p>If he/she scores 84 points below, the minimum discount will automatically be given to</br> 
-                any student who took the initiative to take the examination. This scholarship grant is good</br> 
-                for 1 semester only.</p>
+                <p>An examination is being given to high school graduates whose grades in all subjects during the 4th grading period of their last year
+                in high school are not lower than 85%. However, consideration is being given to students who have a single grade lower than 85%
+                in a particular subject while the remaining subjects fully compensate for the deficit.</p>
+
+            <p>If he/she scores 84 points below, the minimum discount will automatically be given to any student who took the initiative to
+            take the examination. This scholarship grant is good for 1 semester only.</p>
+
+            <h3>Scheme:</h3>
+            <ul>
+                <li>Valedictorian – 100% tuition fee discount</li>
+                <li>Salutatorian – 50% tuition fee discount</li>
+                <li>First Honorable Mention – 25% tuition fee discount</li>
+                <li>Second Honorable Mention – 20% tuition fee discount</li>
+                <li>Third Honorable Mention – 15% tuition fee discount</li>
+                <li>Fourth Honorable Mention – 10% tuition fee discount</li>
+                <li>Fifth Honorable Mention – 10% tuition fee discount</li>
+            </ul>
+
+            <h3>Academics Scholarship / Deans Lister</h3>
+            <ul>
+                <li>Applicant must be a bonafide, regular student taking regular load prescribed in the curriculum.</li>
+                <li>Any applicant whether graduating or not, maybe a candidate for honors. They must not have grades lower than 1.75 or its numerical equivalent of 90 in all academic subjects in any term and in any year level.</li>
+                <li>Applicant must have grades not lower than 2.25 or its equivalent number of 84 in PE & NSTP.</li>
+            </ul>
+
+            <h3>GWA Earned Discount Scheme Total No. of Units Enrolled</h3>
+            <ul>
+                <li>GWA of 1.50 (93-95) – 25%</li>
+                <li>GWA of 1.25 (96-98) – 50%</li>
+                <li>GWA of 1.00 (99-100) – 100%</li>
+            </ul>
                 </div>
             </div>
         </div>
