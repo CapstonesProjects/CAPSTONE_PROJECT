6c722a52e9bed7c784a1e8b62ad6a83fe69d640e
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-10-05 03:37:40 +0800

    new update

--- Events.php
+++ Events.php
@@ -23,13 +23,32 @@
     <section id="events" class="events">
         <div class="events-container">
             <div class="bg-container">
-            <div class="dep-img">
-         
+                <div class="slider-wrapper">
+                    <div class="slider">
+                        <img id="slide1"src="images/Event/image1.jpg"/>
+                        <img id="slide2"src="images/Event/image2.jpg"/>
+                        <img id="slide3"src="images/Event/image3.jpg"/>
+                        <img id="slide4"src="images/Event/image4.jpg"/>
+                        <img id="slide6"src="images/Event/image6.jpg"/>
+                        <img id="slide7"src="images/Event/image7.jpg"/>
+                        <img id="slide8"src="images/Event/image8.jpg"/>
+                        <img id="slide9"src="images/Event/image9.jpg"/>
+                        <img id="slide10"src="images/Event/image10.jpg"/>
+                    </div>
                 </div>
 
-                <div class="text-content">
-
+                <div class="slide-nav">  
+                    <a herf="slide1"></a>
+                    <a herf="slide2"></a>
+                    <a herf="slide3"></a>
+                    <a herf="slide4"></a>
+                    <a herf="slide6"></a>
+                    <a herf="slide7"></a>
+                    <a herf="slide8"></a>
+                    <a herf="slide9"></a>
+                    <a herf="slide10"></a>
                 </div>
+
             </div>
         </div>
     </section>
