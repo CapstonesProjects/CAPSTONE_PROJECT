98536eb3f06411e8ee2f5306c01cf0e20e94c2ee
Perdon-ShannaMae-BSIT32E1 <145529377+Perdon-ShannaMae-BSIT32E1@users.noreply.github.com>
2024-11-02 02:19:53 +0800

    rules and regulations progress

---
+++ components/student_rnr.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<style>
+#rnr {
+  display: flex;
+  height: 190vh;
+  justify-content: center;
+  align-items: center;
+  padding: 110px;
+}
+
+
+
+.bg-container {
+  width: 100%;
+  max-width: 1400px;
+  height: auto;
+  padding: 3rem;
+  background: #ffff;
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border-radius: 10px;
+  border: 1px solid rgba(18, 18, 18, 0.1);
+  box-shadow: 10px 20px 20px rgba(0, 0, 0, 0.3);
+  text-align: center;
+}
+</style>
+
+<body>
+
+
+    <section id="rnr" class="rnr">
+        <div class="scholar-container">
+            <div class="bg-container">
+
+                <div class="text-content">
+                <h1 class="scholar-title">Scholarships and Grants</h1>
+                <section class="scho-info">
+                <p class="sent-p">An examination is being given to high school graduates whose grades in all subjects during the 4th grading period of their last year
+                in high school are not lower than 85%. However, consideration is being given to students who have a single grade lower than 85%
+                in a particular subject while the remaining subjects fully compensate for the deficit.</p>
+
+            <p>If he/she scores 84 points below, the minimum discount will automatically be given to any student who took the initiative to
+            take the examination. This scholarship grant is good for 1 semester only.</p>
+            </section>
+
+            
+            <h1 class="title-sub">Scheme:</h1>
+            <section class="scheme">
+            <ul>
+                <li>Valedictorian – 100% tuition fee discount</li>
+                <li>Salutatorian – 50% tuition fee discount</li>
+                <li>First Honorable Mention – 25% tuition fee discount</li>
+                <li>Second Honorable Mention – 20% tuition fee discount</li>
+                <li>Third Honorable Mention – 15% tuition fee discount</li>
+                <li>Fourth Honorable Mention – 10% tuition fee discount</li>
+                <li>Fifth Honorable Mention – 10% tuition fee discount</li>
+            </ul>
+            </section>
+
+            
+            <h1 class="title-sub">Academics Scholarship / Deans Lister</h1>
+            <section class="scho-info">
+            <ul>
+                <p>Applicant must be a bonafide, regular student taking regular load prescribed in the curriculum.
+                Any applicant whether graduating or not, maybe a candidate for honors. They must not have grades lower than 1.75 or its numerical equivalent of 90 in all academic subjects in any term and in any year level.
+                Applicant must have grades not lower than 2.25 or its equivalent number of 84 in PE & NSTP.</p>
+            </ul>
+            </section>
+
+            
+            <h1 class="title-sub">GWA Earned Discount Scheme Total No. of Units Enrolled</h1>
+            <section class="scheme">
+            <ul>
+                <li>GWA of 1.50 (93-95) – 25%</li>
+                <li>GWA of 1.25 (96-98) – 50%</li>
+                <li>GWA of 1.00 (99-100) – 100%</li>
+            </ul>
+            </section>
+
+                </div>
+
+            <div class="dep-img">
+                <h1 class="title-sub">Departments</h1>
+                <div class="dep-logo">
+            <img src="images/Scho/CCS.png" class="dep-imgi">
+            <img src="images/Scho/CE.png" class="dep-imgi">
+            <img src="images/Scho/CCJ.png" class="dep-imgi">
+            <img src="images/Scho/COAAS.png" class="dep-imgi">
+            <img src="images/Scho/COE.png" class="dep-imgi">
+            <img src="images/Scho/BME.png" class="dep-imgi">
+            <img src="images/Scho/REM.png" class="dep-imgi">
+            <img src="images/Scho/TAHM.png" class="dep-imgi">
+            <img src="images/Scho/BE.png" class="dep-imgi">
+            </div>
+            </div>
+
+            </div>
+        </div>
+    </section>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+
+</html>
