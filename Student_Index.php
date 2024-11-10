46965f3f1ae962907c9c5b96797163fbfe24e527
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-11-10 16:03:27 +0800

    update of the header of index student and organization humburger

--- Student_Index.php
+++ Student_Index.php
@@ -19,6 +19,7 @@ include('./alerts/login_alerts.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/login_form.css">
+    <link rel="stylesheet" href="css/responsiveness_header.css">
     <link rel="shortcut icon" href="images/osa_logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -97,11 +98,13 @@ include('./alerts/login_alerts.php');
     <?php include('./alerts/login_alerts.php') ?>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="./javascript/sessionmessage.js"></script>
-<script src="./javascript/active.js"></script>
-<script src="javascript/alerts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="javascript/hamburger_menu.js"></script>
+    <script src="./javascript/sessionmessage.js"></script>
+    <script src="./javascript/active.js"></script>
+    <script src="javascript/alerts.js"></script>
+
 
 </html>
