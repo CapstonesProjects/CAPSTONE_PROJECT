3b2cacb34442de02539cf43484d5f9992ca21b97
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-11-10 16:09:40 +0800

    osa text resize per device

--- components/osa_sidebar.php
+++ components/osa_sidebar.php
@@ -229,6 +229,10 @@
                 width: 20rem;
                 display: block;
             }
+
+            .text-md {
+                font-size: 18px; /* Adjust this size as needed */
+            }
         }
 
         
@@ -265,6 +269,9 @@
                 transition: background-color 0.3s ease; /* Smooth transition for background color */
             }
 
+            .text-md {
+                font-size: 15px; /* Adjust this size as needed */
+            }
 
         }
 
@@ -300,6 +307,10 @@
                 border-radius: 30px; /* Rounded corners */
                 transition: background-color 0.3s ease; /* Smooth transition for background color */
             }
+
+            .text-md {
+                font-size: 12px; /* Adjust this size as needed */
+            }
         }
     </style>
 </head>
@@ -334,13 +345,13 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.5em" height="0.5em" viewBox="0 0 24 24">
                             <path fill="currentColor" d="M13 9V3h8v6zM3 13V3h8v10zm10 8V11h8v10zM3 21v-6h8v6z" />
                         </svg>
-                        <span class="text-sm">Dashboard</span>
+                        <span class="text-sm sm:text-md md:text-lg">Dashboard</span>
                     </a>
                     <a href="../OSA/OSA_Profile.php" class="text-lg font-medium <?php echo $activeMenu == 'osa_profile' ? 'active' : 'text-gray-700'; ?> py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.5em" height="0.5em" viewBox="0 0 24 24">
                             <path fill="currentColor" fill-rule="evenodd" d="M8 7a4 4 0 1 1 8 0a4 4 0 0 1-8 0m0 6a5 5 0 0 0-5 5a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3a5 5 0 0 0-5-5z" clip-rule="evenodd" />
                         </svg>
-                        <span class="text-sm">Profile</span>
+                        <span class="text-sm sm:text-md md:text-lg">Profile</span>
                     </a>
                     <!-- <a href="../OSA/OSA_Statistics.php" class="text-lg font-medium <?php echo $activeMenu == 'statistics' ? 'active' : 'text-gray-700'; ?> py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 26 26">
@@ -352,13 +363,13 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.5em" height="0.5em" viewBox="0 0 24 24">
                             <path fill="currentColor" d="M3 22q-.825 0-1.412-.587T1 20V9h2v11h17v2zm4-4q-.825 0-1.412-.587T5 16V5h5V3q0-.825.588-1.412T12 1h4q.825 0 1.413.588T18 3v2h5v11q0 .825-.587 1.413T21 18zm5-13h4V3h-4z" />
                         </svg>
-                        <span class="text-sm">Cases</span>
+                        <span class="text-sm sm:text-md md:text-lg">Cases</span>
                     </a>
                     <a href="../OSA/OSA_Monitoring.php" class="text-lg font-medium <?php echo $activeMenu == 'sanctionmanagement' ? 'active' : 'text-gray-700'; ?> py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.5em" height="0.5em" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
                             <path d="M18.277 8c.347.596.985 1 1.723 1a2 2 0 0 0 0-4c-.738 0-1.376.404-1.723 1H16V4a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H5.723C5.376 5.404 4.738 5 4 5a2 2 0 0 0 0 4c.738 0 1.376-.404 1.723-1H8v.369C5.133 9.84 4.318 12.534 4.091 14H3a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-.877c.197-.959.718-2.406 2.085-3.418A.984.984 0 0 0 9 11h6a.98.98 0 0 0 .792-.419c1.373 1.013 1.895 2.458 2.089 3.419H17a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-1.092c-.227-1.466-1.042-4.161-3.908-5.632V8h2.277zM6 18H4v-2h2v2zm14 0h-2v-2h2v2zm-6-9h-4V5h4v4z"></path>
                         </svg>
-                        <span class="text-sm">Monitoring</span>
+                        <span class="text-sm sm:text-md md:text-lg">Monitoring</span>
                     </a>
                     <a href="../OSA/OSA_Send_Message.php" class="text-lg font-medium <?php echo $activeMenu == 'send_message' ? 'active' : 'text-gray-700'; ?> py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.5em" height="0.5em" viewBox="0 0 24 24">
@@ -367,13 +378,13 @@
                                 <path d="M8.023 17.215q.05-.046.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785" />
                             </g>
                         </svg>
-                        <span class="text-sm">Send Message</span>
+                        <span class="text-sm sm:text-md md:text-lg">Send Message</span>
                     </a>
                     <a href="../OSA/OSA_StudentProfile.php" class="text-lg font-medium <?php echo $activeMenu == 'student_profile' ? 'active' : 'text-gray-700'; ?> py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.5em" height="0.5em" viewBox="0 0 24 24">
                             <path fill="currentColor" d="M12 5.5A3.5 3.5 0 0 1 15.5 9a3.5 3.5 0 0 1-3.5 3.5A3.5 3.5 0 0 1 8.5 9A3.5 3.5 0 0 1 12 5.5M5 8c.56 0 1.08.15 1.53.42c-.15 1.43.27 2.85 1.13 3.96C7.16 13.34 6.16 14 5 14a3 3 0 0 1-3-3a3 3 0 0 1 3-3m14 0a3 3 0 0 1 3 3a3 3 0 0 1-3 3c-1.16 0-2.16-.66-2.66-1.62a5.54 5.54 0 0 0 1.13-3.96c.45-.27.97-.42 1.53-.42M5.5 18.25c0-2.07 2.91-3.75 6.5-3.75s6.5 1.68 6.5 3.75V20h-13zM0 20v-1.5c0-1.39 1.89-2.56 4.45-2.9c-.59.68-.95 1.62-.95 2.65V20zm24 0h-3.5v-1.75c0-1.03-.36-1.97-.95-2.65c2.56.34 4.45 1.51 4.45 2.9z" />
                         </svg>
-                        <span class="text-sm">Student Profile</span>
+                        <span class="text-sm sm:text-md md:text-lg">Student Profile</span>
                     </a>
                     <!-- <a href="../OSA/OSA_Events.php" class="text-lg font-medium <?php echo $activeMenu == 'events' ? 'active' : 'text-gray-700'; ?> py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
@@ -395,7 +406,7 @@
                                 <path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path>
                                 <path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path>
                             </svg>
-                            <span>Logout</span>
+                            <span class="text-sm sm:text-md md:text-lg">Logout</span>
                         </button>
                     </form>
                 </div>
