83293bf198fa6756b03509e745a03e9a4413d55a
GerodiazRogel-BSIT32E1 <160821545+GerodiazRogel-BSIT32E1@users.noreply.github.com>
2024-11-12 19:32:52 +0800

    try to fix conflict

--- components/student_case.php
+++ components/student_case.php
@@ -50,7 +50,7 @@ while ($row = $result->fetch_assoc()) {
 
 <!-- Modal for Case Details -->
 <div id="case-details-modal" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" style="display: none;">
-    <div id="modal-container" class="relative top-20 mx-auto p-5 border w-1/2  rounded-md bg-white">
+    <div  class="relative top-20 mx-auto p-5 border w-1/2  rounded-md bg-white">
         <div class="mt-3 text-center">
             <h3 class="text-lg leading-6 font-medium text-gray-900" id="case-id"></h3>
             <div class="mt-2">
